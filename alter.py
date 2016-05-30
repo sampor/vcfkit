ce0f46e181eb84e5076e0688e638108ccb9ac770
Daniel Danis <daniel.danis.farmacia@gmail.com>
2016-05-31 00:00:30 +0200

    Create alter.Alternator, implement _Call creating methods Create tests

--- alter.py
+++ alter.py
@@ -1,10 +1,123 @@
-from vcf.model import make_calldata_tuple
+from vcf import Reader
+from vcf.model import make_calldata_tuple, _Call
+from vcf.parser import RESERVED_FORMAT
 
 
-def create_call(sample_id, **kwargs):
-    keys = list(kwargs.keys())
-    nt = make_calldata_tuple(keys)
+class Alternator(object):
+    """Class for convenient altering of VCF records, especially _Call and _CallData objects
+
+    As _Call and CallData objects are immutable this class simplifies creation of new Calls and CallDatas. It may be
+    useful for reorganizing and formatting VCF records, e.g. moving important values from custom format fields
+    to more common fields which are reckognized by most software. (E.g. CGA_CEHQ to HQ in case of Complete Genomics
+    VCF files."""
+
+    def __init__(self, reader_handle):
+        self.rhandle = reader_handle
+
+    # def
+
+    def create_empty_call_data(self, samp_fmt_s):
+        """Create CallData object as in method vcf.parser._parse_samples
+        :param samp_fmt_s: VCF Record format string
+        :return: CallData named tuple
+        """
+        samp_fmt = make_calldata_tuple(samp_fmt_s.split(':'))
+
+        for fmt in samp_fmt._fields:
+            try:
+                entry_type = self.rhandle.formats[fmt].type
+                entry_num = self.rhandle.formats[fmt].num
+            except KeyError:
+                entry_num = None
+                try:
+                    entry_type = RESERVED_FORMAT[fmt]
+                except KeyError:
+                    entry_type = 'String'
+            samp_fmt._types.append(entry_type)
+            samp_fmt._nums.append(entry_num)
+        return samp_fmt
+
+    def create_call_data(self, samp_fmt):
+        """Create CallData object to hold values of samples' FORMAT field
+
+        :return:
+        """
+        if samp_fmt not in self.rhandle._format_cache:
+            self.rhandle._format_cache[samp_fmt] = self.create_empty_call_data(samp_fmt)
+        samp_fmt = self.rhandle._format_cache[samp_fmt]
+        return samp_fmt
+
+    def create_calls(self, samples, samp_fmt, site):
+        """Create a list of _Call object as it is found in vcf.model._Record.data attribute.
+
+        Method is not very inovative, it repeats vcf.parser._parse_samples .
+        :param samples: list of strings with samples' data (vcf columns 9+)
+        :param samp_fmt: vcf FORMAT string
+        :param site: _Record object
+        :return: list of _Call objects
+        """
+        smp_fmt = self.create_call_data(samp_fmt)
+
+        samp_data = []
+        _map = self.rhandle._map
+        nfields = len(smp_fmt._fields)
+
+        for name, sample in zip(self.rhandle.samples, samples):
+
+            sampdat = [None] * nfields
+
+            for i, vals in enumerate(sample.split(':')):
+
+                if smp_fmt._fields[i] == 'GT':
+                    sampdat[i] = vals
+                    continue
+                elif not vals or vals == '.':
+                    sampdat[i] = None
+                    continue
+
+                entry_num = smp_fmt._nums[i]
+                entry_type = smp_fmt._types[i]
+
+                # we don't need to split single entries
+                if entry_num == 1 or ',' not in vals:
+
+                    if entry_type == 'Integer':
+                        try:
+                            sampdat[i] = int(vals)
+                        except ValueError:
+                            sampdat[i] = float(vals)
+                    elif entry_type == 'Float':
+                        sampdat[i] = float(vals)
+                    else:
+                        sampdat[i] = vals
+
+                    if entry_num != 1:
+                        sampdat[i] = (sampdat[i])
+
+                    continue
+
+                vals = vals.split(',')
+
+                if entry_type == 'Integer':
+                    try:
+                        sampdat[i] = _map(int, vals)
+                    except ValueError:
+                        sampdat[i] = _map(float, vals)
+                elif entry_type == 'Float' or entry_type == 'Numeric':
+                    sampdat[i] = _map(float, vals)
+                else:
+                    sampdat[i] = vals
+
+            # create a call object
+            call = _Call(site, name, smp_fmt(*sampdat))
+            samp_data.append(call)
+
+        return samp_data
 
 
 if __name__ == '__main__':
     pass
+
+in_vcf = '/home/daniel/ielis/vcfkit/input/FH327.snp.vcf'
+alt = Alternator(Reader(filename=in_vcf))
+fmt = 'GT:AD:DP:PL'
---
+++ test/test_alternator.py
@@ -0,0 +1,45 @@
+import alter
+import unittest
+
+try:
+    from vcf.parser import Reader
+    from vcf.model import _Call
+except ImportError:
+    raise Exception("PyVCF is not installed. Try '$ sudo pip3 install pyvcf'")
+
+
+class TestAlternator(unittest.TestCase):
+    def setUp(self):
+        self.alternator = alter.Alternator(Reader(filename='files/XYZ123.vcf'))
+        self.one_hom = next(self.alternator.rhandle)
+        self.x_het = next(self.alternator.rhandle)
+        self.x_monoploid = next(self.alternator.rhandle)
+        self.y_monoploid = next(self.alternator.rhandle)
+        self.fmt_string = self.x_het.FORMAT
+
+    def test_create_empty_call_data(self):
+        ecd = self.alternator.create_empty_call_data(self.fmt_string)
+        self.assertEqual(ecd._fields[0], 'GT')
+        self.assertEqual(ecd._types[0], 'String')
+        self.assertEqual(ecd._types[-1], 'Integer')
+        self.assertEqual(ecd._nums[0], 1)
+        self.assertEqual(ecd._nums[-2], 2)
+        self.assertIsInstance(ecd.AD, property)
+
+    def test_create_call_data(self):
+        cd = self.alternator.create_call_data(self.fmt_string)
+        self.assertEqual(cd._fields[0], 'GT')
+        self.assertEqual(cd._types[-1], 'Integer')
+        self.assertEqual(cd._nums[-2], 2)
+        self.assertIsInstance(cd.DP, property)
+
+    def test_create_calls(self):
+        sample_data = '1/0:.:PASS:433:470,433:470,433:41,44:-470,0,-433:-41,0,-44:48:25,23:23'
+        cs = self.alternator.create_calls([sample_data], self.fmt_string, self.x_het)
+        self.assertIsInstance(cs, list)
+        self.assertIsInstance(cs[0], _Call)
+        self.assertEqual(cs[0].data.GT, '1/0')
+        self.assertEqual(cs[0].sample, 'XYZ123')
+
+    if __name__ == '__main__':
+        unittest.main()
